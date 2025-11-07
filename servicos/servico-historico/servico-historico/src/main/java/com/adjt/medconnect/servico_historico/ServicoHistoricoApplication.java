2bda9e8ce448f1b5208fb884ed529bbf7b37ae1d
cauana <cauana.dias@hotmail.com>
2025-11-06 22:53:32 -0300

    Estrutura inicial do MedConnect-PosTech

---
+++ servicos/servico-historico/servico-historico/src/main/java/com/adjt/medconnect/servico_historico/ServicoHistoricoApplication.java
@@ -0,0 +1,13 @@
+package com.adjt.medconnect.servico_historico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoHistoricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoHistoricoApplication.class, args);
+	}
+
+}
