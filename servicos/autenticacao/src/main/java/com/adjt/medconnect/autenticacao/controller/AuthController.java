b178d39226b2a25bcce438804db9ef5e0bc7bf23
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-12-16 01:13:30 -0300

    Configuração rota

--- servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/controller/AuthController.java
+++ servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/controller/AuthController.java
@@ -1,9 +1,18 @@
 package com.adjt.medconnect.autenticacao.controller;
 
+import com.adjt.medconnect.autenticacao.dto.LoginDTO;
+import com.adjt.medconnect.autenticacao.dto.TokenDTO;
+import com.adjt.medconnect.autenticacao.model.Role;
 import com.adjt.medconnect.autenticacao.model.Usuario;
 import com.adjt.medconnect.autenticacao.repository.UsuarioRepository;
+import com.adjt.medconnect.autenticacao.service.JwtService;
+import com.adjt.medconnect.autenticacao.service.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,21 +22,39 @@ import java.net.URI;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final PasswordEncoder encoder;
-    private final UsuarioRepository repository;
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UsuarioService usuarioService;
 
     @PostMapping("/register")
-    public ResponseEntity<Usuario> register(@RequestBody Usuario usuario){
-        if(repository.findByUsername(usuario.getUsername()).isPresent()){
-            return ResponseEntity.badRequest().build();
-        }
-        usuario.setPassword(encoder.encode(usuario.getPassword()));
-        Usuario saved = repository.save(usuario);
-        return ResponseEntity.created(URI.create("/auth" + saved.getId())).body(saved);
+    public ResponseEntity<Void> register(@RequestBody Usuario usuario) {
+        usuario.setRole(Role.PACIENTE); // força role segura
+        usuarioService.salvar(usuario);
+
+        return ResponseEntity.created(
+                URI.create("/auth/" + usuario.getId())
+        ).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> login(@Valid @RequestBody LoginDTO dto) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        dto.getUsername(),
+                        dto.getPassword()
+                )
+        );
+
+        String token = jwtService.gerarToken(authentication);
+        
+
+        return ResponseEntity.ok(new TokenDTO(token));
     }
 
     @GetMapping("/me")
-    public ResponseEntity<String> me(){
-        return ResponseEntity.ok("Você está autenticado");
+    public ResponseEntity<String> me(Authentication auth) {
+        return ResponseEntity.ok("Você está autenticado: " + auth.getName());
     }
 }
