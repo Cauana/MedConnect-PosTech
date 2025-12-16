48a7a7e1e88111c011918c667e8044616d12de97
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-12-16 01:12:06 -0300

    Regras segurança chamadas roles

--- servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/config/SecurityConfig.java
+++ servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.adjt.medconnect.autenticacao.config;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import com.adjt.medconnect.autenticacao.filter.JwtFilter;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import com.adjt.medconnect.autenticacao.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
@@ -11,42 +16,41 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final UsuarioService usuarioService;
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/medicos/**").hasRole("MEDICO")
-                        .requestMatchers("/enfermeiros/**").hasRole("ENFERMEIRO")
-                        .requestMatchers("/pacientes/**").hasRole("PACIENTE")
+                        .requestMatchers("/auth/login","/auth/register").permitAll()
+                        .requestMatchers("/medico/**").hasRole("MEDICO")
+                        .requestMatchers("/enfermeiro/**").hasRole("ENFERMEIRO")
+                        .requestMatchers("/paciente/**").hasRole("PACIENTE")
                         .anyRequest().authenticated()
                 )
-                .authenticationProvider(authenticationProvider())
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(usuarioService);
-                authProvider.setPasswordEncoder(passwordEncoder());
-                return authProvider;
-
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+
 }
+
