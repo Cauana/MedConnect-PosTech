dd17ec70f212931b1cc4d29105d91e1fa9ec0a2e
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-12-16 01:19:21 -0300

    Implementa UsuarioService para gerenciar usuários e autenticação

--- servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/service/UsuarioService.java
+++ servicos/autenticacao/src/main/java/com/adjt/medconnect/autenticacao/service/UsuarioService.java
@@ -6,24 +6,33 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService implements UserDetailsService {
-    private final UsuarioRepository repository;
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioService(UsuarioRepository repository) {
-        this.repository = repository;
+    public UsuarioService(UsuarioRepository repository,
+                          PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-        Usuario usuario = repository.findByUsername(username)
+        Usuario usuario = usuarioRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
         return User.builder()
                 .username(usuario.getUsername())
                 .password(usuario.getPassword())
                 .roles(usuario.getRole().name())
                 .build();
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+        return usuarioRepository.save(usuario);
     }
 }
